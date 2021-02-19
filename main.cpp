66c6be909906fd05b67d49a5f53dd1623422c0f6
Darth Reggie <yousefway@gmail.com>
2021-02-19 17:36:08 -0500

    window code
    tried to get a window going but the damn thing gave me 7 errors

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 #include <SDL.h>
 
+
 SDL_Window* g_pWindow = 0;
 SDL_Renderer* g_pRenderer = 0;
 
 int main(int argc, char* args[])
 {
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 255);
+
+	SDL_RenderClear(g_pRenderer);
+
+	SDL_RenderPresent(g_pRenderer);
+
+	SDL_Delay(15000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	return 0; 
+}
