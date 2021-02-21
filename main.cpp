8b0f210bf83029151e745e5f22119353f548b658
Darth Reggie <yousefway@gmail.com>
2021-02-20 20:40:34 -0500

    changed the delay and maybe one other thing
    i changed the delayed and put i the title mood killers can't figure out how we gotta render the sprites

--- main.cpp
+++ main.cpp
@@ -1,18 +1,19 @@
 #include <SDL.h>
+#include <iostream>
 
 
-SDL_Window* g_pWindow = 0;
-SDL_Renderer* g_pRenderer = 0;
 
 int main(int argc, char* args[])
 {
 	SDL_Init(SDL_INIT_EVERYTHING);
+	SDL_Window* window = SDL_CreateWindow("Mood Killers", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 600, 400, SDL_WINDOW_SHOWN);
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, 0);
 
-	SDL_SetRenderDrawColor(g_pRenderer, 0, 0, 255, 255);
+	SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
 
-	SDL_RenderClear(g_pRenderer);
+	SDL_RenderClear(renderer);
 
-	SDL_RenderPresent(g_pRenderer);
+	SDL_RenderPresent(renderer);
 
 	SDL_Delay(15000);
 
